23e9a0b91427e5b2a930dfb4384d247706cb3e18
Ravi Chaudhary <ravi@lcx.com>
2023-05-01 13:03:45 +0530

    testing redirection

--- src/pages/Pending.js
+++ src/pages/Pending.js
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+
 const Pending = () => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      window.location = "https://www.lcx.com/";
+    }, [5000]);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
   return <div>Hello from pending</div>;
 };
 
